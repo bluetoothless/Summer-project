eb3e71f9606a9b57a0948a8c39677987d01451f9
bluetoothless <sersaw@onet.pl>
2022-08-10 18:40:45 +0200

    Worker update
    Worker update

--- backend/WorkerService/Worker.cs
+++ backend/WorkerService/Worker.cs
@@ -7,29 +7,6 @@ using WorkerService.Services;
 
 namespace WorkerService
 {
-    class MyConsumer : DefaultBasicConsumer
-    {
-        ILogger<Worker> _logger;
-        public MyConsumer(IModel model, ILogger<Worker> logger) : base(model) 
-        { _logger = logger; }
-        
-        public override void HandleBasicDeliver(string consumerTag, ulong deliveryTag, bool redelivered, string exchange, string routingKey, IBasicProperties properties, ReadOnlyMemory<byte> body)
-        {
-            var message = Encoding.UTF8.GetString(body.ToArray());
-
-            if (properties.Headers != null)
-            {
-                string student = Encoding.UTF8.GetString((byte[])properties.Headers["student"]);
-                int index = (int)properties.Headers["index"];
-                _logger.LogInformation($"{student} {index} {message}");
-            }
-            else
-            {
-                _logger.LogInformation(message);
-            }
-            Model.BasicAck(deliveryTag, false);
-        }
-    }
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
@@ -44,8 +21,8 @@ namespace WorkerService
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            //var rabbitMqConnection = new RabbitMqConnection();
-            //rabbitMqConnection.Connect();
+            var rabbitMqConnection = new RabbitMqConnection();
+            rabbitMqConnection.Connect();
 
             while (!stoppingToken.IsCancellationRequested)
             {
@@ -64,41 +41,7 @@ namespace WorkerService
                     DisplayUserInformation(users);
                 }
 
-
-
-                Console.WriteLine("Backend");
-
-                string fromWebQName = "fromWebQueue";
-                factory = new ConnectionFactory()
-                {
-                    UserName = "xzghooiz",
-                    Password = "W_2R6sH6OLg5WU86J0upqHdStyDIUyyW",
-                    HostName = "hawk.rmq.cloudamqp.com",
-                    VirtualHost = "xzghooiz"
-                };
-
-                var message = "";
-                using (connection = factory.CreateConnection())
-                using (channel = connection.CreateModel())
-                {
-                    channel.QueueDeclare(fromWebQName, true, false, false, null);
-                    var consumer = new MyConsumer(channel, _logger);
-                    channel.BasicQos(0, 1, false);
-                    channel.BasicConsume(fromWebQName, false, consumer);
-                    /*_logger.LogInformation("Tu");
-                        var consumer = new EventingBasicConsumer(channel);
-                        consumer.Received += (sender, e) =>
-                        {
-                            var body = e.Body.ToArray();
-                            message = Encoding.UTF8.GetString(body);
-                            _logger.LogInformation(message);
-                        };
-                        channel.BasicConsume("fromWebQueue", true, consumer);*/
-                }
-
-
-
-                //var message = rabbitMqConnection.ReceiveMessage(_logger);
+                var message = rabbitMqConnection.ReceiveMessage(_logger);
                 if (message != "")
                 {
                     _logger.LogInformation(message);
